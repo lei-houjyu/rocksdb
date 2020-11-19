19dc2d126777542083be30a569b3ae838421128e
Changxu Luo <changxu.luo@columbia.edu>
2020-11-19 14:01:13 -0500

    modified:   db/db_impl/db_impl_open.cc

--- db/db_impl/db_impl_open.cc
+++ db/db_impl/db_impl_open.cc
@@ -1463,13 +1463,13 @@ Status DB::Open(const DBOptions& db_options, const std::string& dbname,
   const bool kSeqPerBatch = true;
   const bool kBatchPerTxn = true;
   // ASH: changes to add db_paths
-  DBOptions temp_db_options = db_options;
-  temp_db_options.statistics = CreateDBStatistics();
-  temp_db_options.stats_dump_period_sec = 60;
-  temp_db_options.db_paths.push_back(rocksdb::DbPath("/mnt/sdb/archive_dbs/sst_dir/sst_last_run", 10000000000));
-  return DBImpl::Open(temp_db_options, dbname, column_families, handles, dbptr,
-                      !kSeqPerBatch, kBatchPerTxn);
-//  return DBImpl::Open(db_options, dbname, column_families, handles, dbptr, !kSeqPerBatch, kBatchPerTxn);
+//  DBOptions temp_db_options = db_options;
+//  temp_db_options.statistics = CreateDBStatistics();
+//  temp_db_options.stats_dump_period_sec = 60;
+//  temp_db_options.db_paths.push_back(rocksdb::DbPath("/mnt/sdb/archive_dbs/sst_dir/sst_last_run", 10000000000));
+//  return DBImpl::Open(temp_db_options, dbname, column_families, handles, dbptr,
+//                      !kSeqPerBatch, kBatchPerTxn);
+  return DBImpl::Open(db_options, dbname, column_families, handles, dbptr, !kSeqPerBatch, kBatchPerTxn);
 }
 
 IOStatus DBImpl::CreateWAL(uint64_t log_file_num, uint64_t recycle_log_number,
