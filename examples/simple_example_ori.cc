ae353b89dfd04e49178d7da496728af49d0ef846
root <root@node-0.lsm-c6220-2.lsm-rep-pg0.apt.emulab.net>
2021-02-26 09:50:27 -0700

    nit

---
+++ examples/simple_example_ori.cc
@@ -0,0 +1,46 @@
+// Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under both the GPLv2 (found in the
+//  COPYING file in the root directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in the root directory).
+
+#include <cstdio>
+#include <string>
+
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+
+using namespace ROCKSDB_NAMESPACE;
+
+#if defined(OS_WIN)
+std::string kDBPath = "C:\\Windows\\TEMP\\rocksdb_simple_example";
+#else
+std::string kDBPath = "/tmp/rocksdb_primary_test";
+#endif
+
+int main() {
+  DB* db;
+  Options options;
+  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+  options.IncreaseParallelism();
+  options.OptimizeLevelStyleCompaction();
+  // create the DB if it's not already present
+  options.create_if_missing = true;
+
+  // open DB
+  Status s = DB::Open(options, kDBPath, &db);
+  assert(s.ok());
+
+  // Put key-value
+  for(int i = 0; i < 5000; ++i) {
+    std::string key_str = std::to_string(i);
+    std::string value = std::to_string(i);
+    s = db->Put(WriteOptions(), key_str, value);
+    std::cout << "Put " << key_str << "-" << value;
+  }
+  assert(s.ok());
+
+  // delete db;
+
+  return 0;
+}
