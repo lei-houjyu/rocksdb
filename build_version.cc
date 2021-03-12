34094c6c6dad9ceacf2c7a3bea417a082e540c02
axe <xz2872@columbia.edu>
2021-03-12 10:16:25 -0700

    CMakeList modified: add build_version.cc

--- build_version.cc
+++ build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@ae353b89dfd04e49178d7da496728af49d0ef846@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2021/02/27 04:34:51@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@3afbcc10966b73720bc353daacedbcbb0e31dd2b@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2021/03/03 14:18:20@";
 const char* rocksdb_build_compile_date = __DATE__;
