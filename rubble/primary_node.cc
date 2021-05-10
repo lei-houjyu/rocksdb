f4a8bdec770e704b5a9d061702733b5d09a40e7a
cc4351 <cc4351@columbia.edu>
2021-05-10 07:55:49 -0600

    fix ip addresses

--- rubble/primary_node.cc
+++ rubble/primary_node.cc
@@ -9,13 +9,13 @@
 
 int main(int argc, char** argv) {
   
-  const std::string primary_server_address = "128.110.153.244:50051";
+  const std::string primary_server_address = "0.0.0.0:50051";
   if(argc != 2){
     std::cout << "Usage: ./program secondary_addr(example: 10.10.1.2:50050)\n";
     return 0;
   }
 
-  const std::string remote_sst_dir= "/mnt/remote/archive_dbs/sst_dir/";
+  const std::string remote_sst_dir= "/mnt/sdb/remote/archive_dbs/sst_dir/";
   const std::string secondary_server_address= argv[1];
   // secondary server is running on localhost:50050
   rocksdb::DB* primary = GetDBInstance("/tmp/rubble_primary", "/mnt/sdb/archive_dbs/primary/sst_dir", remote_sst_dir, secondary_server_address, false, true, false);
--- rubble/tail_node.cc
+++ rubble/tail_node.cc
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
   if (argc == 2) {
       target_addr = argv[1];
   }
-  const std::string server_addr = "128.110.153.244:50052"; 
+  const std::string server_addr = "0.0.0.0:50052"; 
   rocksdb::DB* db = GetDBInstance("/tmp/rubble_tail", "/mnt/sdb/archive_dbs/tail/sst_dir","", target_addr, false, false, true);
 
   RunServer(db, server_addr, 16);
