808001407b4f69f6de5713ac4585ac3f007102bf
cc4351 <cc4351@columbia.edu>
2021-10-13 07:58:26 -0600

    update secondary node driver code

--- rubble/secondary_node.cc
+++ rubble/secondary_node.cc
@@ -7,18 +7,23 @@
  */
 int main(int argc, char** argv) {
 
+  const std::string primary_server_address = "0.0.0.0:50053";
   if(argc != 2){
-    std::cout << "usage: ./program tail_node_addr(example: 10.10.1.2:50049)\n";
+    std::cout << "Usage: ./program secondary_addr(example: 10.10.1.2:50050)\n";
     return 0;
   }
 
-  std::string tail_node_addr = argv[1];
+  // const std::string remote_sst_dir= "/mnt/nvme1n1p4/archive_dbs/tail/sst_dir";
+  const std::string remote_sst_dir= "/mnt/sdb/archive_dbs/tail/sst_dir/";
+  const std::string secondary_server_address= argv[1];
   const std::string db_path = "/mnt/sdb/archive_dbs/secondary/db";
   const std::string sst_path = "/mnt/sdb/archive_dbs/secondary/sst_dir";
-  const std::string remote_sst_dir = "/mnt/nvme1n1p4/archive_dbs/tail/sst_dir";
-  rocksdb::DB* secondary = GetDBInstance(db_path, sst_path,remote_sst_dir, tail_node_addr, true, false, false);
-  
-  //secondary server running on port 50051
-  RunServer(secondary, "0.0.0.0:50051");
+  rocksdb::DB* primary = GetDBInstance(db_path, sst_path, remote_sst_dir, secondary_server_address, false, false, false);
+  bool is_async = false;
+  if(is_async){
+    RunAsyncServer(primary, primary_server_address);
+  }else{
+    RunServer(primary, primary_server_address);
+  }
   return 0;
 }
