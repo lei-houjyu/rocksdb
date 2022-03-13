23c49ed1889ac1bd40bbd776cf222d129caa908f
Haoyu Li <haoyu.li@columbia.edu>
2022-03-13 14:17:40 -0600

    Adjust limit_in_bytes in create_cgroups.sh

--- build_version.cc
+++ build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@8f755950134ae8e31f022cd886d8373a2c40f110@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@39a5d53320eda51298f0f0e72515f56b074eec86@";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2021/12/27 19:49:20@";
 const char* rocksdb_build_compile_date = __DATE__;
--- rubble/create_cgroups.sh
+++ rubble/create_cgroups.sh
@@ -3,6 +3,6 @@
 cgcreate -g memory:/rubble-mem
 cgcreate -g cpuset:/rubble-cpu
 
-cgset -r memory.limit_in_bytes=1G rubble-mem
+cgset -r memory.limit_in_bytes=2G rubble-mem
 cgset -r cpuset.cpus=0-7 rubble-cpu
 cgset -r cpuset.mems=0 rubble-cpu
