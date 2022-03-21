400814d6cbc8dbbb4d23f385b0dcda3e43fd296f
Haoyu Li <haoyu.li@columbia.edu>
2022-03-21 00:45:27 -0600

    Set default value of is_rubble to true in both ini files

--- build_version.cc
+++ build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@e17111f5f3581b0afbf8abd98edca717b988310c@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2021/12/27 19:49:20@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@2bf810df3ac86eb3d7b941f6d6b4f1f4e7d28f0b@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2022/03/21 05:43:02@";
 const char* rocksdb_build_compile_date = __DATE__;
