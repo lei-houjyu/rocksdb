d9cb5d341015b24344cc1a250f0c40f126c97e58
Haoyu Li <haoyu.li@columbia.edu>
2022-04-12 14:17:52 -0600

    Uncomment create-sst-pool.sh

--- rubble/create-sst-pool.sh
+++ rubble/create-sst-pool.sh
@@ -13,15 +13,15 @@ padding=1048576
 
 cd $sst_dir
 
-#for i in $(seq 1 $pool_size); do
-#    target_file_size=`expr $target_file_size_base + $padding`
-#    echo $i
-#    if [ -f "$i" ] && [ `ls -l $i | awk '{print $5}'` == $target_file_size ] && [ "$(filefrag -e $i | grep unwritten)" == "" ]; then
-#        continue
-#    fi
-#    rm $i
-#    head -c $target_file_size /dev/zero > $i
-#done
+for i in $(seq 1 $pool_size); do
+   target_file_size=`expr $target_file_size_base + $padding`
+   echo $i
+   if [ -f "$i" ] && [ `ls -l $i | awk '{print $5}'` == $target_file_size ] && [ "$(filefrag -e $i | grep unwritten)" == "" ]; then
+       continue
+   fi
+   rm $i
+   head -c $target_file_size /dev/zero > $i
+done
 
 n=`expr $pool_size + 1`
 for t in $(seq 2 $max_num_mems_in_flush); do
