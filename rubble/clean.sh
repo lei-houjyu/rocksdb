2a8d2511f1ba608520df1b1b24b16a5e1a3bf2e8
Haoyu Li <haoyu.li@columbia.edu>
2022-07-19 14:38:22 -0600

    Update clean.sh, create_cgroups.sh, and .gitignore

--- rubble/clean.sh
+++ rubble/clean.sh
@@ -2,6 +2,7 @@ if [ $# != 1 ]; then
   echo "Usage: bash clean.sh shard_num"
 fi
 
+rm -rf log/* core*
 for i in $(seq $1); do
   rm -rf /mnt/db/$i/primary/*
   rm -rf /mnt/db/$i/tail/*
--- rubble/create_cgroups.sh
+++ rubble/create_cgroups.sh
@@ -3,6 +3,6 @@
 cgcreate -g memory:/rubble-mem
 cgcreate -g cpuset:/rubble-cpu
 
-cgset -r memory.limit_in_bytes=4G rubble-mem
+cgset -r memory.limit_in_bytes=8G rubble-mem
 cgset -r cpuset.cpus=0-3 rubble-cpu
 cgset -r cpuset.mems=0 rubble-cpu
