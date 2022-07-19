fb5439b17d61a98e92c40890879b79cd54bc7e15
Haoyu Li <haoyu.li@columbia.edu>
2022-07-19 14:40:41 -0600

    git rm some auto-generated files

--- build_version.cc
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-#include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@d9cb5d341015b24344cc1a250f0c40f126c97e58@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2022/04/14 00:47:19@";
-const char* rocksdb_build_compile_date = __DATE__;
