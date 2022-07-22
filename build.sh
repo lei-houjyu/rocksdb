5f6ca4f86dc58de1609d087245b62f3d26fc75a5
Haoyu Li <haoyu.li@columbia.edu>
2022-07-22 17:11:10 -0600

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+ROCKSDB_DIR=/mnt/code/my_rocksdb
+
+cd ${ROCKSDB_DIR}
+rm -rf cmake-build-debug CMakeCache.txt CMakeFiles cmake_install.cmake
+cd ${ROCKSDB_DIR}/rubble
+rm -rf cmake-build-debug CMakeCache.txt CMakeFiles cmake_install.cmake
+
+cd ${ROCKSDB_DIR}
+cmake .
+make clean
+make -j48
+
+cd ${ROCKSDB_DIR}/rubble
+cmake .
+make clean
+make -j48
+
+
