113766bb713c24a0bf985c48c64291eb6915cca4
Haoyu Li <haoyu.li@columbia.edu>
2022-07-25 14:23:20 -0600

    Add save-result.sh

---
+++ rubble/save-result.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+    echo "Usage: bash save-result.sh suffix"
+    exit
+fi
+
+suffix=$1
+cp primary-1.out primary-1-${suffix}.out
+cp primary-2.out primary-2-${suffix}.out
+cp tail-1.out tail-1-${suffix}.out
+cp tail-2.out tail-2-${suffix}.out
+cp dstat.csv dstat-${suffix}.csv
+cp iostat.out iostat-${suffix}.out
+cp /mnt/db/1/primary/db/LOG LOG-primary-1-${suffix}
+cp /mnt/db/2/primary/db/LOG LOG-primary-2-${suffix}
+cp /mnt/db/1/tail/db/LOG LOG-tail-1-${suffix}
+cp /mnt/db/2/tail/db/LOG LOG-tail-2-${suffix}
