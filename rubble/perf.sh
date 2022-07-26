9c4b27dc23ce93af2e7e081fc6e81a79465085cd
Haoyu Li <haoyu.li@columbia.edu>
2022-07-26 08:32:16 -0600

    Update perf.sh

--- rubble/perf.sh
+++ rubble/perf.sh
@@ -9,7 +9,7 @@ name=$1
 port=$2
 pid=`ps aux | grep "$name $port" | awk '{print $2}' | head -n 1`
 
-perf record -p $pid -F 499 -C 0-3 -g
+perf record -F 99 -a -g -- sleep 120
 perf script > ${name}.perf
 /mnt/code/FlameGraph/stackcollapse-perf.pl ${name}.perf > ${name}.folded
 /mnt/code/FlameGraph/flamegraph.pl ${name}.folded > ${name}.svg
