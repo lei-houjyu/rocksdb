24901eb7a7c470667ddcca7e2a0b3febab88eb2d
Haoyu Li <haoyu.li@columbia.edu>
2022-07-27 12:21:27 -0600

    Update perf.sh

--- rubble/perf.sh
+++ rubble/perf.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ $# != 2 ]; then
-  echo "Usage: bash perf.sh name port"
+if [ $# != 3 ]; then
+  echo "Usage: bash perf.sh name cpuset time"
   exit
 fi
 
 name=$1
-port=$2
-pid=`ps aux | grep "$name $port" | awk '{print $2}' | head -n 1`
+#port=$2
+#pid=`ps aux | grep "$name $port" | awk '{print $2}' | head -n 1`
+cpu=$2
+time=$3
 
-perf record -F 99 -a -g -- sleep 120
+perf record -F 99 -C ${cpu} -g -- sleep ${time}
 perf script > ${name}.perf
 /mnt/code/FlameGraph/stackcollapse-perf.pl ${name}.perf > ${name}.folded
 /mnt/code/FlameGraph/flamegraph.pl ${name}.folded > ${name}.svg
