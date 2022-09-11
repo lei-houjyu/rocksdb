68546eb423dfb8c4a386c9fc9621f50e91d824b0
Haoyu Li <haoyu.li@columbia.edu>
2022-09-11 14:38:22 -0600

    Refine plot-iostat.py

--- rubble/plot-iostat.py
+++ rubble/plot-iostat.py
@@ -5,6 +5,13 @@ file_name = sys.argv[1]
 agg_num = int(sys.argv[2])
 figure_name = file_name[:-4]
 data = {'user':[0], 'nice':[0], 'system':[0], 'iowait':[0], 'steal':[0], 'idle':[0]}
+ignore_key = ['code', 'swap', 'nvme']
+
+def should_ignore(key):
+    for i in ignore_key:
+        if i in key:
+            return True
+    return False
 
 agg_cnt = 0
 with open(file_name, 'r') as f:
@@ -26,14 +33,15 @@ with open(file_name, 'r') as f:
             line = f.readline()
             while len(line) > 0 and line[0] == 'n':
                 nums = line.split()
-                read_key  = nums[0] + '-read'
-                write_key = nums[0] + '-write'
-                if read_key not in data:
-                    data[read_key] = [0]
-                if write_key not in data:
-                    data[write_key] = [0]                    
-                data[read_key][-1] += float(nums[2]) / 1024
-                data[write_key][-1] += float(nums[3]) / 1024
+                if not should_ignore(nums[0]):
+                    read_key  = nums[0] + '-read'
+                    write_key = nums[0] + '-write'
+                    if read_key not in data:
+                        data[read_key] = [0]
+                    if write_key not in data:
+                        data[write_key] = [0]                    
+                    data[read_key][-1] += float(nums[2]) / 1024
+                    data[write_key][-1] += float(nums[3]) / 1024
                 line = f.readline()
             if agg_cnt % agg_num == 0:
                 for key in data:
