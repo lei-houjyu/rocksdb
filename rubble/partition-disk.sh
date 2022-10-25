ccb2c18118c5c6ebbc9a382107df1994fcae0362
Haoyu Li <haoyu.li@columbia.edu>
2022-10-25 17:21:25 -0400

    Update partition-disk.sh

--- rubble/partition-disk.sh
+++ rubble/partition-disk.sh
@@ -5,7 +5,7 @@ sfdisk /dev/nvme0n1 < partition.dump
 yes | mkfs.ext4 /dev/nvme0n1p1
 yes | mkfs.ext4 /dev/nvme0n1p2
 
-mkdir /mnt/db /mnt/sst
+mkdir /mnt/data /mnt/sst
 
-mount /dev/nvme0n1p1 /mnt/db
+mount /dev/nvme0n1p1 /mnt/data
 mount /dev/nvme0n1p2 /mnt/sst
