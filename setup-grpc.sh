ed76657dfa5a91a6db02a54381c883e7f6c9cff5
Haoyu Li <haoyu.li@columbia.edu>
2022-10-25 17:39:56 -0400

    Create setup-grpc.sh

---
+++ setup-grpc.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -x
+
+RUBBLE_PATH='/mnt/data`'
+
+for i in "$@"
+do
+case $i in
+    -p=*|--rubble-path=*)
+    RUBBLE_PATH="${i#*=}"
+    shift # past argument=value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+GPRC_VERSION=1.34.0
+NUM_JOBS=`nproc`
+
+export MY_INSTALL_DIR=/root
+mkdir -p $MY_INSTALL_DIR
+
+export PATH="$PATH:$MY_INSTALL_DIR/bin"
+
+apt-get install -y build-essential autoconf libtool pkg-config libgflags-dev dstat sysstat cgroup-tools cmake python3-pip nvme-cli numactl linux-tools-generic linux-tools-`uname -r`
+
+cd ${RUBBLE_PATH}
+
+if [ ! -d './grpc' ]; then
+    git clone --recurse-submodules -b v${GPRC_VERSION} https://github.com/grpc/grpc
+fi
+cd grpc
+mkdir -p cmake/build
+pushd cmake/build
+cmake -DgRPC_INSTALL=ON \
+    -DgRPC_BUILD_TESTS=OFF \
+    -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+    ../..
+make -j${NUM_JOBS}
+make install
+popd
+
+echo "grpc build success, building hellp world example "
+
+cd ${RUBBLE_PATH}/grpc/examples/cpp/helloworld
+mkdir -p cmake/build
+pushd cmake/build
+cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..
+make -j
+
+echo "export PATH=/root:/root/bin:$PATH" >> /root/.bashrc
+
+echo "hello world example build success"
