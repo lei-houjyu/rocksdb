0a506cdfff80e317a03db8d39ca3358f76510cf7
Haoyu Li <haoyu.li@columbia.edu>
2022-10-25 22:17:50 -0400

    Update setup-keys.sh

--- rubble/setup-keys.sh
+++ rubble/setup-keys.sh
@@ -2,18 +2,19 @@
 
 # From http://docs.cloudlab.us/advanced-topics.html#%28part._geni-get-key%29
 setup_key() {
+  DIR=$1
+  
   # Retrieve the server-generated RSA private key.
-  geni-get key > $HOME/.ssh/id_rsa
-  chmod 600 $HOME/.ssh/id_rsa
+  geni-get key > $DIR/.ssh/id_rsa
+  chmod 600 $DIR/.ssh/id_rsa
 
   # Derive the corresponding public key portion.
-  ssh-keygen -y -f $HOME/.ssh/id_rsa > $HOME/.ssh/id_rsa.pub
+  ssh-keygen -y -f $DIR/.ssh/id_rsa > $DIR/.ssh/id_rsa.pub
 
   # If you want to permit login authenticated by the auto-generated key,
   # then append the public half to the authorized_keys file:
-  grep -q -f $HOME/.ssh/id_rsa.pub $HOME/.ssh/authorized_keys || cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+  grep -q -f $DIR/.ssh/id_rsa.pub $DIR/.ssh/authorized_keys || cat $DIR/.ssh/id_rsa.pub >> $DIR/.ssh/authorized_keys
 }
 
-setup_key
-sudo -i
-setup_key
+setup_key $HOME
+setup_key /root
