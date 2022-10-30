7ca3ab1e6522654cea70796f9aaa8a6f886ca866
Haoyu Li <haoyu.li@columbia.edu>
2022-10-29 23:16:57 -0400

    Report average and max memory usage

--- rubble/show-mem.sh
+++ rubble/show-mem.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ $# != 2 ]; then
     echo "Usage: bash show-mem.sh pid-file top-file"
     exit
@@ -10,23 +11,23 @@ for i in $pids;
 do
   grep $i $1 | awk '{print "command:", $(NF-3)}'
   awk -v pid=$i '{if ($2==pid) {
-                    v+=$6;
-                    p+=$7;
-                    s+=$8;
-                    vsum+=$6;
-                    psum+=$7;
-                    ssum+=$8;
+                    v=$6;
+                    p=$7;
+                    s=$8;
                   }; 
-                  if ($14=="COMMAND") {
+                  if ($14=="COMMAND" && v != 0 && p != 0) {
                     t++;
-                    if (vsum>vmax) vmax=vsum;
-                    if (psum>pmax) pmax=psum;
-                    if (ssum>smax) smax=ssum;
-                    vsum=0;
-                    psum=0;
-                    ssum=0;
+                    if (v>vmax) vmax=v;
+                    if (p>pmax) pmax=p;
+                    if (s>smax) smax=s;
+                    vsum+=v;
+                    psum+=p;
+                    ssum+=s;
+                    v=0;
+                    p=0;
+                    s=0;
                   }
                  }END{
-                  print "VIRT:", v/t/1024/1024, vmax/1024/1024, "PHY", p/t/1024/1024, pmax/1024/1024, "SWAP", s/t/1024/1024, smax/1024/1024;
+                  print "VIRT:", vsum/t/1024/1024, vmax/1024/1024, "PHY", psum/t/1024/1024, pmax/1024/1024, "SWAP", ssum/t/1024/1024, smax/1024/1024;
                  }' $2
 done
