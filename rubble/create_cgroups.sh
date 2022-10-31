7f2294fcf640799d60e421b4cec444f4bd67359e
Haoyu Li <haoyu.li@columbia.edu>
2022-10-31 12:30:18 -0400

    Unlimit memory in cgroup

--- rubble/create_cgroups.sh
+++ rubble/create_cgroups.sh
@@ -3,6 +3,6 @@
 cgcreate -g memory:/rubble-mem
 cgcreate -g cpuset:/rubble-cpu
 
-cgset -r memory.limit_in_bytes=8G rubble-mem
-cgset -r cpuset.cpus=0-3 rubble-cpu
+cgset -r memory.limit_in_bytes=256G rubble-mem
+cgset -r cpuset.cpus=0,2,4,6 rubble-cpu
 cgset -r cpuset.mems=0 rubble-cpu
