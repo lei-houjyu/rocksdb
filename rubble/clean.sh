08ba5c1fda8c81c81a28cbbbdd043dca5a89ee22
Haoyu Li <haoyu.li@columbia.edu>
2022-11-08 19:53:25 -0500

    Update clean.sh

--- rubble/clean.sh
+++ rubble/clean.sh
@@ -5,6 +5,8 @@ fi
 rm -rf log/* core*
 for (( i=0; i<$1; i++ )); do
   for (( j=0; j<$2; j++ )); do
-    rm -rf /mnt/data/db/shard-$i-replica-$j.out
+    rm -rf /mnt/data/db/shard-$i/db/* \
+           /mnt/data/db/shard-$i/sst_dir/* \
+           /mnt/data/db/shard-$i-replica-$j.out
   done
 done
