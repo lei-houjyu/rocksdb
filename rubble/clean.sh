2981cdc17a220f74254609be3a155def0f354d8e
Haoyu Li <haoyu.li@columbia.edu>
2022-11-08 20:07:41 -0500

    Update save-result.sh and clean.sh

--- rubble/clean.sh
+++ rubble/clean.sh
@@ -1,12 +1,12 @@
-if [ $# != 2 ]; then
-  echo "Usage: bash clean.sh shard_num rf"
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage: bash clean.sh shard_num"
 fi
 
 rm -rf log/* core*
 for (( i=0; i<$1; i++ )); do
-  for (( j=0; j<$2; j++ )); do
-    rm -rf /mnt/data/db/shard-$i/db/* \
-           /mnt/data/db/shard-$i/sst_dir/* \
-           /mnt/data/db/shard-$i-replica-$j.out
-  done
+  rm -rf /mnt/data/db/shard-$i/db/* \
+          /mnt/data/db/shard-$i/sst_dir/* \
+          /mnt/data/db/shard-$i.out
 done
--- rubble/save-result.sh
+++ rubble/save-result.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-if [ $# != 1 ]; then
-    echo "Usage: bash save-result.sh suffix"
+if [ $# -lt 2 ]; then
+    echo "Usage: bash save-result.sh shard_num suffix"
     exit
 fi
 
-suffix=$1
-cp primary-1.out primary-1-${suffix}.out
-cp primary-2.out primary-2-${suffix}.out
-cp tail-1.out tail-1-${suffix}.out
-cp tail-2.out tail-2-${suffix}.out
+shard_num=$1
+suffix=$2
+
 cp dstat.csv dstat-${suffix}.csv
 cp iostat.out iostat-${suffix}.out
 cp pids.out pids-${suffix}.out
 cp top.out top-${suffix}.out
 
-cp /mnt/data/db/1/primary/db/LOG LOG-primary-1-${suffix}
-cp /mnt/data/db/2/primary/db/LOG LOG-primary-2-${suffix}
-cp /mnt/data/db/1/tail/db/LOG LOG-tail-1-${suffix}
-cp /mnt/data/db/2/tail/db/LOG LOG-tail-2-${suffix}
+for (( i=0; i<$shard_num; i++ ))
+do
+    cp shard-${i}.out shard-${i}-${suffix}.out
+    cp /mnt/data/db/shard-${i}/db/LOG LOG-shard-${i}-${suffix}
+done
