157d322f1336af0858a6b24722fbab5653655353
Haoyu Li <haoyu.li@columbia.edu>
2022-12-08 16:29:50 -0500

    Fix a bug in install-broadcom-driver.sh

--- rubble/install-broadcom-driver.sh
+++ rubble/install-broadcom-driver.sh
@@ -15,10 +15,10 @@ if [ $phase -eq 1 ]; then
     wget https://docs.broadcom.com/docs-and-downloads/ethernet-network-adapters/NXE/BRCM_223.1.96.0/bcm_223.1.96.0.tar.gz
     tar zxvf bcm_223.1.96.0.tar.gz
     cd bcm_223.1.96.0/Linux/Linux_Installer/
-    bash install.sh -i eno12409np1 -a 10.10.1.2 -n 255.255.255.0
+    bash install.sh -i eno12409np1 -a $ip -n 255.255.255.0
     reboot
 else
     cd bcm_223.1.96.0/Linux/Linux_Installer/
-    bash install.sh -i eno12409np1 -a 10.10.1.2 -n 255.255.255.0
+    bash install.sh -i eno12409np1 -a $ip -n 255.255.255.0
     sysctl -p
 fi
