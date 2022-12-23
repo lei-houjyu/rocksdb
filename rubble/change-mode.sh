59fa69b17446cf5cf75e60ccd013dd29f4f83e4a
Haoyu Li <haoyu.li@columbia.edu>
2022-12-22 19:53:51 -0700

    Update max_background_jobs in change-mode.sh. When mode is rubble, the head will have rf times more background threads.

--- rubble/change-mode.sh
+++ rubble/change-mode.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-if [ $# != 1 ]; then
-    echo "Usage: bash change-mode.sh mode"
+if [ $# != 3 ]; then
+    echo "Usage: bash change-mode.sh mode(baseline/rubble) rf base_background_jobs"
     exit
 fi
 
 mode=$1
+rf=$2
+n=$3
+
 if [ $mode == 'rubble' ]; then
     sed -i 's/is_rubble=false/is_rubble=true/g' rubble_16gb_config.ini
     sed -i 's/is_rubble=false/is_rubble=true/g' rubble_16gb_config_tail.ini
+    sed -ir "s/max_background_jobs=[0-9]*/max_background_jobs=$(( n * rf ))/g" rubble_16gb_config.ini
 fi
 
 if [ $mode == 'baseline' ]; then
     sed -i 's/is_rubble=true/is_rubble=false/g' rubble_16gb_config.ini
     sed -i 's/is_rubble=true/is_rubble=false/g' rubble_16gb_config_tail.ini
+    sed -ir "s/max_background_jobs=[0-9]*/max_background_jobs=$n/g" rubble_16gb_config.ini
+    sed -ir "s/max_background_jobs=[0-9]*/max_background_jobs=$n/g" rubble_16gb_config_tail.ini
 fi
