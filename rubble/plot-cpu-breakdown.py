2c5f3c29bc7414052e8753774d2095ef348d98e9
Haoyu Li <haoyu.li@columbia.edu>
2023-01-02 00:17:23 -0700

    Update plot scripts

--- rubble/plot-cpu-breakdown.py
+++ rubble/plot-cpu-breakdown.py
@@ -6,6 +6,7 @@ if len(sys.argv) < 2:
     sys.exit("Usage: python3 plot-compaction-cpu.py suffix")
 
 suffix = sys.argv[1]
+agg_num = int(sys.argv[2])
 
 workload_num = 5
 config_num = 4
@@ -16,7 +17,7 @@ labels = ['Load', 'A', 'B', 'C', 'D']
 
 config = ['baseline-primary', 'baseline-secondary', \
           'rubble-primary',   'rubble-secondary', \
-          'rubble-offload-primary', 'rubble-offload-secondary']
+          'rubble-primary', 'rubble-secondary']
 
 data = {"baseline-compaction-primary"   : [0, 0, 0, 0, 0],
         "baseline-dbserver-primary"     : [0, 0, 0, 0, 0],
@@ -24,26 +25,54 @@ data = {"baseline-compaction-primary"   : [0, 0, 0, 0, 0],
         "baseline-compaction-secondary" : [0, 0, 0, 0, 0],
         "baseline-dbserver-secondary"   : [0, 0, 0, 0, 0],
         "baseline-misc-secondary"       : [0, 0, 0, 0, 0],
-        "rubble-offload-compaction-primary"     : [0, 0, 0, 0, 0],
-        "rubble-offload-dbserver-primary"       : [0, 0, 0, 0, 0],
-        "rubble-offload-misc-primary"           : [0, 0, 0, 0, 0],
-        "rubble-offload-compaction-secondary"   : [0, 0, 0, 0, 0],
-        "rubble-offload-dbserver-secondary"     : [0, 0, 0, 0, 0],
-        "rubble-offload-misc-secondary"         : [0, 0, 0, 0, 0]}
+        "rubble-compaction-primary"     : [0, 0, 0, 0, 0],
+        "rubble-dbserver-primary"       : [0, 0, 0, 0, 0],
+        "rubble-misc-primary"           : [0, 0, 0, 0, 0],
+        "rubble-compaction-secondary"   : [0, 0, 0, 0, 0],
+        "rubble-dbserver-secondary"     : [0, 0, 0, 0, 0],
+        "rubble-misc-secondary"         : [0, 0, 0, 0, 0]}
 
-def count_cpu(word, pid_map, workload, tok):
+time_series_data = {}
+
+def agg(arr):
+    res = []
+    print('len(arr)', len(arr))
+    for i in range(len(arr)):
+        if i % agg_num == 0:
+            if len(res) > 0:
+                res[-1] /= agg_num
+            res.append(arr[i])
+        else:
+            res[-1] += arr[i]
+    res[-1] /= len(arr) % agg_num
+    return res
+
+def count_cpu(word, pid_map, workload, mode, job, time):
     ppid = word[1]
+    category = mode + '-' + job
     cpu = float(word[10]) / 100
-    match = False
     for role in pid_map.keys():
         if ppid in pid_map[role]:
-            tok = tok + '-' + role
+            # 1. for aggregation data
+            tok = category + '-' + role
             data[tok][idx[workload]] += cpu
-            match = True
+
+            # 2. for time series data
+            if job in ['compaction', 'dbserver']:
+                tok = job + '-' + mode + '-' + ppid
+                if tok not in time_series_data.keys():
+                    time_series_data[tok] = [0]
+                else:
+                    if len(time_series_data[tok]) < time:
+                        time_series_data[tok].append(0)
+                    time_series_data[tok][-1] += cpu
+
             break
 
 for workload in labels:
-    for mode in ['baseline', 'rubble-offload']:
+    for mode in ['baseline', 'rubble']:
+        time = 0
+        time_series_data.clear()
         workload = workload.lower()
         top_fname = 'top-' + mode + '-' + workload + '-' + suffix + '.out'
         pid_fname = 'pids-' + mode + '-' + workload + '-' + suffix + '.out'
@@ -65,20 +94,36 @@ for workload in labels:
         # 2. get the CPU utilization data
         with open(top_fname, 'r') as f:
             line = f.readline()
-            time = 0
             while line:
                 word = line.split()
                 if len(word) == 14 and word[0].isnumeric():
                     cmd = word[-1]
-                    if cmd == 'COMMAND':
-                        time += 1
-                    elif cmd.startswith('rocksdb'):
-                        count_cpu(word, pid_map, workload, mode+'-compaction')
+                    if cmd.startswith('rocksdb'):
+                        count_cpu(word, pid_map, workload, mode, 'compaction', time)
                     elif cmd.startswith('grpcpp_sync_ser'):
-                        count_cpu(word, pid_map, workload, mode+'-dbserver')
+                        count_cpu(word, pid_map, workload, mode, 'dbserver', time)
                     else:
-                        count_cpu(word, pid_map, workload, mode+'-misc')
+                        count_cpu(word, pid_map, workload, mode, 'misc', time)
+                elif len(word) == 14 and word[-1] == 'COMMAND':
+                    time += 1
                 line = f.readline()
+
+        # 3. plot time series data
+        plt.figure()
+        time_axis = [i for i in range(int(time / agg_num) + int(time % agg_num > 0))]
+        print('time_axis', len(time_axis))
+        for key in time_series_data.keys():
+            print(key, len(time_series_data[key]))
+            plt.plot(time_axis, agg(time_series_data[key]), label=key)
+        figure_name = 'real-time-cpu-' + mode + '-' + workload + '-' + suffix + '.jpg'
+        print('real time cpu stats are plot in ' + figure_name)
+        plt.xlabel('Time')
+        plt.ylabel('CPU Utilization')
+        plt.ylim([0, 1])
+        plt.legend()
+        plt.savefig(figure_name)
+        plt.close()
+    break
 
 for key in data:
     print(key, ','.join(map(str, data[key])))
--- rubble/plot-thru.py
+++ rubble/plot-thru.py
@@ -3,24 +3,31 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 file_name = sys.argv[1]
-data = {'read':[], 'write':[], 'queued_op':[], 'queued_edit':[], 'time':[]}
+agg_num = int(sys.argv[2])
+data = {'read':[], 'write':[], 'queued_op':[], 'queued_edit':[]}
 
 with open(file_name, 'r') as f:
     line = f.readline()
     
+    cnt = 0
     while line:
         if line.startswith('[READ]') and line.endswith('2022\n'):
             nums = line.split()
             if nums[1] != '0' or nums[6] != '0':
-                data['read'].append(int(nums[3]))
-                data['write'].append(int(nums[8]))
-                data['queued_op'].append(int(nums[13]))
-                data['queued_edit'].append(int(nums[15]))
-
-                time_obj = datetime.strptime(nums[-2], '%H:%M:%S')
-                data['time'].append(time_obj)
+                if cnt == 0:
+                    for key in data:
+                        if len(data[key]) > 0:
+                            data[key][-1] /= agg_num
+                        data[key].append(0)
+                data['read'][-1] += int(nums[3])
+                data['write'][-1] += int(nums[8])
+                data['queued_op'][-1] += int(nums[13])
+                data['queued_edit'][-1] += int(nums[15])
+                cnt = (cnt + 1) % agg_num
 
         line = f.readline()
+
+data['time'] = [i for i in range(len(data['read']))]
 
 plt.figure()
 for key in ['read', 'write']:
@@ -30,6 +37,7 @@ for key in ['read', 'write']:
 plt.xticks(rotation = 45)
 plt.xlabel('Time')
 plt.ylabel('OP/s')
+plt.ylim([0,100000])
 plt.legend()
 plt.savefig(file_name[:-4] + '-thru.jpg')
 print(file_name[:-4] + '-thru.jpg')
