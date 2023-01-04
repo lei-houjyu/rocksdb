d2f526541a813de8ddd18b4b0d095d3e5facdb8f
Haoyu Li <haoyu.li@columbia.edu>
2023-01-04 01:28:23 -0500

    Remove local from verify-sst-pool.sh

--- rubble/verify-sst-pool.sh
+++ rubble/verify-sst-pool.sh
@@ -7,35 +7,35 @@ fi
 
 source helper.sh
 
-local target_file_size_base=$1
-local max_num_mems_in_flush=$2
-local pool_size=$3
-local shard_num=$4
-local rf=$5
-local file_size=$(( target_file_size_base + 1048576 ))
-local local_nid=$( get_nid )
-local res="Clean"
+target_file_size_base=$1
+max_num_mems_in_flush=$2
+pool_size=$3
+shard_num=$4
+rf=$5
+file_size=$(( target_file_size_base + 1048576 ))
+local_nid=$( get_nid )
+res="Clean"
 
 for (( remote_nid=1; remote_nid<=$rf; remote_nid++ )); do
     if [ $remote_nid -ne $local_nid ]; then
         for (( sid=0; sid<$shard_num; sid++ )); do
-            local ret=$( is_head $remote_nid $sid $rf )
+            ret=$( is_head $remote_nid $sid $rf )
             if [ "$ret" == "true" ]; then
-                local shard_dir="/mnt/sst/node-${remote_nid}/shard-$sid"
+                shard_dir="/mnt/sst/node-${remote_nid}/shard-$sid"
                 ret=$( check_sst_pool $shard_dir $file_size )
                 if [ "$ret" == "Bad" ]; then
                     # 0. record error
                     res="Error"
 
                     # 1. umount the disk on remote host
-                    local remote_mount_point="/mnt/remote-sst/node-${local_nid}"
-                    local remote_dev=$( ssh ${USER}@node-${remote_nid} "df -h | grep ${remote_mount_point} | awk '{print \$1}'" )
+                    remote_mount_point="/mnt/remote-sst/node-${local_nid}"
+                    remote_dev=$( ssh ${USER}@node-${remote_nid} "df -h | grep ${remote_mount_point} | awk '{print \$1}'" )
                     ssh ${USER}@node-${remote_nid} "killall db_node; umount ${remote_mount_point}"
 
                     # 2. umount the disk locally
                     killall db_node
-                    local local_mount_point="/mnt/sst/node-${remote_nid}"
-                    local local_dev=$( df -h | grep ${local_mount_point} | awk '{print $1}' )
+                    local_mount_point="/mnt/sst/node-${remote_nid}"
+                    local_dev=$( df -h | grep ${local_mount_point} | awk '{print $1}' )
                     umount $local_mount_point
                     yes | mkfs.ext4 $local_dev
                     mount $local_dev $local_mount_point
@@ -44,7 +44,7 @@ for (( remote_nid=1; remote_nid<=$rf; remote_nid++ )); do
                     for (( i=0; i<$shard_num; i++ )); do
                         ret=$( is_head $remote_nid $i $rf )
                         if [ "$ret" == "true" ]; then
-                            local d="${local_mount_point}/shard-${i}"
+                            d="${local_mount_point}/shard-${i}"
                             bash create-sst-pool.sh $target_file_size_base $max_num_mems_in_flush $pool_size $d > /dev/null 2>&1 &
                         fi
                     done
